f40f507e6469cfb206512c5a8efa9f8dd383e97d
Memória de Cálculo <memoriadcalculo@gmail.com>
2024-11-03 21:08:11 -0300

    Fix NoneType subs

--- sympycharfield/__init__.py
+++ sympycharfield/__init__.py
@@ -22,5 +22,8 @@ def str2sympy(sympy_str):
     for k, v in u.__dict__.items(): 
         if (isinstance(v, Expr) and v.has(u.Unit)) or isinstance(v, Integer): 
             subs[Symbol(k)] = v
-    return sympify(sympy_str).subs(subs)
+    if sympify(sympy_str) is None:
+        return None
+    else:
+        return sympify(sympy_str).subs(subs)
 
